25ed0da499f8025af83f57bf4a39a95d7496fa8c
Alejandro <alejandroc0701@gmail.com>
2018-05-16 05:40:05 -0400

    Commit1
    Version Inicial

---
+++ src/CapaWeb/ProyectoIS2018_2/ProyectoIS2018_2/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProyectoIS2018_2
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MultiView1.ActiveViewIndex = 1;
+        }
+
+        protected void Proyecto1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
